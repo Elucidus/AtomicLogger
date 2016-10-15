53e5a1a562a59356badc2d384aadbe45d938f26b
Elucidus <sharkracer78@gmail.com>
2016-10-15 02:34:33 -0700

    Added an update delay, separated Thread logic to a standalone class and added a random TPS generator thread to stress-test update mechanism for UI.

--- src/com/sharkylabs/GaugeCluster.java
+++ src/com/sharkylabs/GaugeCluster.java
@@ -16,35 +16,16 @@ import javafx.stage.Stage;
 
 public class GaugeCluster extends Application {
 	private static ECMInfo ecmInfo;
-	private static GaugeCanvas canvas; 
+	private static GaugeCanvas canvas;
 	
-	public static void main(String[] args) {
-		
-		Thread t = new Thread() {
+	/**
+	 * The update delay in ms - controls how frequently to fire UX update events.
+	 */
+	private static final int UPDATE_DELAY_MSEC = 50;
 
-			@Override
-			public void run() {
-				try {
-					BufferedReader br = new BufferedReader(new InputStreamReader(
-							System.in));
-					String line;
-					while ((line = br.readLine()) != null) {
-						if (ecmInfo == null) {
-							ecmInfo = new ECMInfo(line);
-						} else {
-							ecmInfo.parseData(line);
-						}
-						if (canvas != null) {
-							//TODO move code to kick off thread to stage init
-							canvas.fireEvent(new ECMUpdateEvent());
-						}
-						ecmInfo.printCurrentData();
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		};
+	public static void main(String[] args) {
+		Thread t = new HammerThread();
+		new PollingThread();
 		t.start();
 		launch(args);
 	}
@@ -55,29 +36,105 @@ public class GaugeCluster extends Application {
 		public ECMUpdateEvent() {
 			super(EVENT_TYPE_ECM_UPDATE);
 		}
-	
-		public static final EventType<ECMUpdateEvent> EVENT_TYPE_ECM_UPDATE = new EventType<>("ECMUpdate");
+
+		public static final EventType<ECMUpdateEvent> EVENT_TYPE_ECM_UPDATE = new EventType<>(
+				"ECMUpdate");
 	}
+
 	@Override
 	public void start(Stage stage) throws Exception {
 		Group root = new Group();
 		Scene scene = new Scene(root, 300, 300, Color.LIGHTGREY);
 		canvas = new GaugeCanvas(640, 480);
-		canvas.addEventHandler(ECMUpdateEvent.EVENT_TYPE_ECM_UPDATE, new EventHandler<ECMUpdateEvent>() {
+		canvas.addEventHandler(ECMUpdateEvent.EVENT_TYPE_ECM_UPDATE,
+				new EventHandler<ECMUpdateEvent>() {
 
-			@Override
-			public void handle(ECMUpdateEvent arg0) {
-				canvas.redraw(); 
-			}
-			
-		});
-		canvas.redraw(); 
-		
+					@Override
+					public void handle(ECMUpdateEvent arg0) {
+						canvas.redraw();
+					}
+
+				});
+		canvas.redraw();
+
 		root.getChildren().add(canvas);
 		stage.setScene(scene);
 		stage.show();
 	}
-	
+
+	/**
+	 * This thread hammers random TPS data - for testing UI latency.
+	 */
+	private static class HammerThread extends Thread {
+		String[] lines = { "can0       301   [8]  00 17 05 15 05 44 06 C6",
+				"can0       301   [8]  00 17 05 15 05 44 06 A2",
+				"can0       301   [8]  00 17 05 15 05 44 06 20",
+				"can0       301   [8]  00 17 05 15 05 44 06 0",
+				"can0       301   [8]  00 17 05 15 05 44 06 20",
+				"can0       301   [8]  00 17 05 15 05 44 06 80",
+				"can0       301   [8]  00 17 05 15 05 44 06 B2", };
+
+		@Override
+		public void run() {
+			int currentLine = 0;
+			long lastFiredEvent = 0;
+			while (true) {
+				if (ecmInfo == null) {
+					ecmInfo = new ECMInfo(lines[currentLine]);
+				} else {
+					ecmInfo.parseData(lines[currentLine]);
+				}
+				currentLine++;
+				if (currentLine == lines.length) {
+					currentLine = 0;
+				}
+				long currentTime = System.currentTimeMillis();
+				if (canvas != null && ((currentTime - lastFiredEvent) > UPDATE_DELAY_MSEC)) {
+					// TODO move code to kick off thread to stage init
+					lastFiredEvent = currentTime;
+					canvas.fireEvent(new ECMUpdateEvent());
+				}
+				try {
+					sleep(UPDATE_DELAY_MSEC);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	/**
+	 * This thread polls stdin for CAN data line by line.
+	 */
+	private static class PollingThread extends Thread {
+		@Override
+		public void run() {
+			try {
+				long lastFiredEvent = 0;
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						System.in));
+				String line;
+				while ((line = br.readLine()) != null) {
+					if (ecmInfo == null) {
+						ecmInfo = new ECMInfo(line);
+					} else {
+						ecmInfo.parseData(line);
+					}
+					long currentTime = System.currentTimeMillis();
+					if (canvas != null && ((currentTime - lastFiredEvent) > UPDATE_DELAY_MSEC)) {
+						// TODO move code to kick off thread to stage init
+						lastFiredEvent = currentTime;
+						canvas.fireEvent(new ECMUpdateEvent());
+					}
+//					ecmInfo.printCurrentData();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	private class GaugeCanvas extends Canvas {
 		public GaugeCanvas(int width, int height) {
 			super(width, height);
@@ -91,7 +148,7 @@ public class GaugeCluster extends Application {
 			if (ecmInfo != null) {
 				gc.setFill(Color.RED);
 				gc.fillRect(10, 10, ecmInfo.throttlePosition, 25);
-			}	
+			}
 		}
 	}
 
