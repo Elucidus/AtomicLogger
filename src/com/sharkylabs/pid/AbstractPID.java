457516c362933ddcca034470ff1c5d7704c5c3e9
Elucidus <sharkracer78@gmail.com>
2016-10-17 23:30:09 -0700

    Switch to StringBuffer for displaying PID data - decrease String churn.

--- src/com/sharkylabs/pid/AbstractPID.java
+++ src/com/sharkylabs/pid/AbstractPID.java
@@ -11,7 +11,7 @@ public abstract class AbstractPID {
 	public int minValue;
 	public int maxValue;
 	
-	
+	protected StringBuffer displayBuffer = new StringBuffer(20);
 	/**
 	 * Used to set the value of a given PID. This method will convert the CAN 
 	 * encoded representation to a real world value.
@@ -23,6 +23,16 @@ public abstract class AbstractPID {
 	 * PIDs generate a string in the format of: $name[$value$unit]
 	 */
 	public String toString() {
-		return this.name + "[" + this.currentValue + this.unit + "]";
+		if (displayBuffer.length() > 0) {
+			displayBuffer.delete(this.name.length() + 1, displayBuffer.length());
+		} else {
+			displayBuffer.append(this.name);
+			displayBuffer.append('[');
+		}
+		displayBuffer.append(this.currentValue);
+		displayBuffer.append(this.unit);
+		displayBuffer.append(']');
+		
+		return displayBuffer.toString();
 	}
 }
